57137f54f9118807bb47573e99d0c9413a9b60b1
davay <davay@defucc.me>
2024-04-04 17:32:27 +0700

    video icon

--- theme/components/EventCard.vue
+++ theme/components/EventCard.vue
@@ -9,7 +9,7 @@ const date = useDateFormat(() => props.event?.date, 'DD MMM YYYY')
 </script>
 
 <template lang='pug'>
-NuxtLink.p-0.flex.flex-wrap.items-start.gap-4.rounded-xl.overflow-clip.max-w-150(
+NuxtLink.p-0.flex.flex-wrap.items-start.gap-4.rounded-xl.overflow-clip.max-w-150.relative(
   :to="`/events/${event.slug}/`"
   ) 
   .p-0.max-h-30(style="flex: 1 1 400px")
@@ -20,6 +20,7 @@ NuxtLink.p-0.flex.flex-wrap.items-start.gap-4.rounded-xl.overflow-clip.max-w-150
       :src="event?.cover"
       v-if="event?.cover"
       )
+    .i-la-play-circle.op-80.text-light-400.text-80px.absolute.top-8(style="left:calc(50% - 50px)" v-if="event?.youtube_video || event?.live_stream")
   .flex.flex-col.flex-auto.gap-2.glass.p-2.mx-2.mb-2(style="flex: 1 1 400px")
     .text-md {{ date }}
     .text-xl {{ event?.title }}
